b606c86c75d06b64750f705b60b60c0c56be13c1
Jason Butwell <jasonbutwell@gmail.com>
2016-11-03 22:53:23 +0000

    Background drawn, and bird drawn in middle of screen. Bird animates using deltatime calculation.

--- core/src/com/jasonbutwell/flappybirdclone/FlappyBirdClone.java
+++ core/src/com/jasonbutwell/flappybirdclone/FlappyBirdClone.java
@@ -1,5 +1,6 @@
 package com.jasonbutwell.flappybirdclone;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,20 +9,44 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBirdClone extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background, bird;
+	Texture birds[];
+	int flapState = 0;
+
+	float dt;
+
 	@Override
 	public void create () {
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
+
+		bird = new Texture("bird.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		dt += Gdx.graphics.getDeltaTime();
+
+		if ( dt >= 0.1 ) {
+			dt = 0;
+			if (flapState == 0)
+				flapState = 1;
+			else
+				flapState = 0;
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+		batch.draw(birds[flapState], (Gdx.graphics.getWidth()-bird.getWidth())/2, (Gdx.graphics.getHeight()-bird.getHeight())/2);
 		batch.end();
 	}
 }
