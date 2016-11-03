58c8b2ded4255fb91e87ff81f8d74dc0bddb76eb
Jason Butwell <jasonbutwell@gmail.com>
2016-11-03 22:14:18 +0000

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# FlappBirdClone
+# FlappBirdClone - A quick and simple implentation of the Flappy Bird game using the libGDX game development library and Android Studio.
---
+++ desktop/src/com/jasonbutwell/flappybirdclone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jasonbutwell.flappybirdclone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jasonbutwell.flappybirdclone.FlappyBirdClone;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBirdClone(), config);
+	}
+}
